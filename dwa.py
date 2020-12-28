42a4f6a3465a0a57635aff26f0ccc1fb89debb2a
gaileyleseman <gaileyleseman@gmail.com>
2020-12-28 12:41:10 +0100

    robot and obstacle class

--- dwa.py
+++ dwa.py
@@ -1,4 +1,5 @@
 import yaml
+import math
 
 class Params:
     def __init__(self, config):
@@ -11,4 +12,30 @@ def get_params():
         config = yaml.load(f, Loader=yaml.FullLoader)
         p = Params(config)
     return
+
+class Robot:
+    def __init__(self, start_pos, params):
+        # initialize robot state
+        self.x = start_pos[0]
+        self.y = start_pos[1]
+        self.theta = self.v = self.omega = 0
+        self.p = params
+
+    def update_state(self, v, omega):
+        self.v = v
+        self.omega = omega
+        self.theta += self.omega * self.p.dt
+        if self.omega == 0:     # straight line
+            self.x += self.v * math.cos(self.theta) * self.p.dt
+            self.y += self.omega + math.sin(self.theta) * self.p.dt
+        else:                   # circular trajectory
+            self.x += (self.v/self.omega) * (math.sin(self.theta) + math.sin(self.theta + self.omega * self.p.dt))
+            self.y += -(self.v/self.omega) * (math.cos(self.theta) + math.cos(self.theta + self.omega * self.p.dt))
+
+
+class Obstacle:
+    def __init__(self, x, y, r):
+        self.x = x
+        self.y = y
+        self.r = r
 
